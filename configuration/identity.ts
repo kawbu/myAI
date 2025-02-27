c64362a779fcb0a9e8606134c332d730ae9894b6
kawbu <kawbu0823@gmail.com>
2025-02-27 17:41:25 -0500

    Updated Title Screen (ui.ts) and OWNER_DESCRIPTION and AI_TONE to be 'helpful and friendly'.

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,6 +1,6 @@
 export const OWNER_NAME: string = `Kaw Thang Bu`;
-export const OWNER_DESCRIPTION: string = `Goofy cs student`;
+export const OWNER_DESCRIPTION: string = `Charismatic Second Year CS university student at the University Of Chapel Hill, Cultural Chair for the South East Asian Student Association (SEASA) at UNC.`;
 
 export const AI_NAME: string = `不卡 AI`;
-export const AI_TONE: string = `[AI TONE]`;
+export const AI_TONE: string = `Helpful and friendly`;
 export const AI_ROLE: string = `[AI ROLE]`;
--- configuration/ui.ts
+++ configuration/ui.ts
@@ -4,7 +4,7 @@ export const CHAT_HEADER: string = `不卡 AI`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
 export const FOOTER_MESSAGE: string = `Customized AI by Kaw Bu @ UNC`;
 export const CLEAR_BUTTON_TEXT: string = `Erase Chat History`;
-export const PAGE_TITLE: string = `[PAGE TITLE]`;
+export const PAGE_TITLE: string = `Kaw AI`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
